4c67c5ca8a1730e08f2ae988add0b622b811372d
alirast <xdabnormal@gmail.com>
2023-09-14 22:03:31 +0300

    group simple example

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -9,23 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
   
-  @State private var selectedView = 1
-  
   var body: some View {
-    TabView(selection: $selectedView) {
-      Text("First view")
-        .tabItem {
-          Image(systemName: "1.circle")
-          Text("First")
-        }
-        .tag(1)
-      
-      Text("Second view")
-        .tabItem {
-          Image(systemName: "2.circle")
-          Text("Second")
-        }
-        .tag(2)
+    VStack {
+      Group {
+        Text("hey")
+        Text("hey")
+        Text("hey")
+        Text("hey")
+      }
+      Group {
+        Text("hey")
+        Text("hey")
+        Text("hey")
+        Text("hey")
+      }
+      Group {
+        Text("hey")
+        Text("hey")
+        Text("hey")
+        Text("hey")
+      }
     }
   }
 }
