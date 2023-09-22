cdfae68f43d5cdccd8cac4bde69b220f8fdd5754
alirast <xdabnormal@gmail.com>
2023-09-22 21:38:32 +0300

    list with sections

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -7,31 +7,31 @@
 
 import SwiftUI
 
-struct Restaurant: Identifiable {
-  var id = UUID()
-  var name: String
-}
-
-struct RestaurantRow: View {
-  var restaurant: Restaurant
-  
+struct TaskRow: View {
   var body: some View {
-    Text("Come and eat at \(restaurant.name)")
+    Text("Some task")
   }
 }
 
 struct ContentView: View {
   
   var body: some View {
-    let first = Restaurant(name: "First Restaurant")
-    let second = Restaurant(name: "Second Restaurant")
-    let third = Restaurant(name: "Third Restaurant")
-    
-    let restaurants = [first, second, third]
-    
-    return List(restaurants) { restaurant in
-      RestaurantRow(restaurant: restaurant)
+    List {
+      Section(header: Text("Important tasks"),
+              footer: Text("End")) {
+        TaskRow()
+        TaskRow()
+        TaskRow()
+      }
+      
+      Section(header: Text("Other tasks")) {
+        TaskRow()
+        TaskRow()
+        TaskRow()
+      }
+      .listRowBackground(Color.yellow)
     }
+    .listStyle(.grouped)
   }
 }
 
