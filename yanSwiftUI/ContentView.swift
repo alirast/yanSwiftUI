4176ee08bf3eb947d1b17a77a216eff11265cebc
alirast <xdabnormal@gmail.com>
2023-09-29 21:22:46 +0300

    long press gesture

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -12,21 +12,15 @@ struct ContentView: View {
   @State private var age = 18
   
   var body: some View {
-    VStack {
-      Image("people")
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-      //how much times we need to tap to execute the code
-        .onTapGesture(count: 2) {
-          print("Double tapped")
-        }
-      Spacer()
-      Text("hello people")
-    }
-    .contentShape(Rectangle())
-    .onTapGesture {
-      print("hello apple")
-    }
+    Image("people")
+      .resizable()
+      .aspectRatio(contentMode: .fit)
+      .gesture(
+        LongPressGesture(minimumDuration: 2)
+          .onEnded { _ in
+            print("pressed")
+          }
+      )
   }
 }
 
