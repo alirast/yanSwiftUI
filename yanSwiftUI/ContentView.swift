55ebf906d5e8dc176a54b530e78aad3b79380054
alirast <xdabnormal@gmail.com>
2023-10-05 21:13:55 +0300

    tap part 1

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -12,17 +12,10 @@ struct ContentView: View {
   @State private var age = 18
   
   var body: some View {
-    VStack {
-      Stepper("Enter ur age", onIncrement: {
-        self.age += 1
-        debugPrint("adding to age")
-      }, onDecrement: {
-        self.age -= 1
-        debugPrint("Subtracting from age")
-      })
-      .padding()
-      Text("ur age is \(age)")
-    }
+    Text("tap me")
+      .onTapGesture {
+        print("tapped")
+      }
   }
 }
 
