e208038bca65c38b1eced559b1ed520d477c2986
alirast <xdabnormal@gmail.com>
2023-10-06 20:31:41 +0300

    state + stepper part 2

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-
+  
   @State private var age = 18
   
   var body: some View {
     VStack {
-      Stepper("Enter ur age", value: $age, in: 0...130)
-        .padding()
-      Text("Ur age is \(age)")
+      Stepper("Enter ur age", onIncrement: {
+        self.age += 1
+        debugPrint("adding to age")
+      }, onDecrement: {
+        self.age -= 1
+        debugPrint("Subtracting from age")
+      })
+      .padding()
+      Text("ur age is \(age)")
     }
   }
 }
