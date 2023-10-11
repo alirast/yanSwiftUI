fd0f3a3b39e49cf510decbc313e22e2e4588391a
alirast <xdabnormal@gmail.com>
2023-10-11 20:16:46 +0300

    state + picker

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
 
-  @State private var celsius: Double = 0
+  private var colors = ["black", "white", "red", "green", "blue"]
+  @State private var selectedColor = 0
   
   var body: some View {
     VStack {
-      Slider(value: $celsius, in: -100...100, step: 0.1)
-        .padding()
-      Text("\(celsius) Celsius is \(celsius * 9 / 5 + 32) Fahrenheit")
+      
+      Picker(selection: $selectedColor) {
+        
+        ForEach(0..<colors.count) { colorIndex in
+          Text(self.colors[colorIndex])
+        }
+        
+      } label: {
+        Text("choose a color")
+      }
+      .pickerStyle(.segmented)
+      .padding()
+      
+      Text("You selected \(colors[selectedColor])")
     }
   }
 }
