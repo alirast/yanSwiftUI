f2c7c27c69cae44523e5ab8bfe78390df524132c
alirast <xdabnormal@gmail.com>
2023-11-05 15:35:26 +0300

    Initial Commit

---
+++ yanSwiftUI/yanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  yanSwiftUIApp.swift
+//  yanSwiftUI
+//
+//  Created by N S on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct yanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
