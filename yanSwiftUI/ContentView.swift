ebd4db704718e824cb0542443ceca81bfead512f
alirast <xdabnormal@gmail.com>
2023-11-06 14:40:05 +0300

    tip calculator

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -9,34 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
   
-  @State private var colors = ["red", "green", "blue"]
-  @State private var selectedColor = 0
-  @State private var additionalSettings = false
+  @State private var checkAmount = ""
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 0
   
+  let tipPercentages = [0, 5, 10, 15, 20]
+  
+  private var totalPerPerson: Double {
+    let peopleCount = Double(numberOfPeople + 2)
+    let tipSelection = Double(tipPercentages[tipPercentage])
+    let orderAmount = Double(checkAmount) ?? 0
+    let tipValue = orderAmount / 100 * tipSelection
+    let grandTotal = orderAmount + tipValue
+    let amountPerPerson = grandTotal / peopleCount
+    
+    return amountPerPerson
+  }
+
   var body: some View {
     NavigationView {
       Form {
-        Section(header: Text("Colors")) {
-          Picker(selection: $selectedColor, label: Text("Select a color")) {
-            ForEach(0..<colors.count) {
-              Text(self.colors[$0])
+        Section {
+          TextField("Amount", text: $checkAmount)
+          Picker("Number of people", selection: $numberOfPeople) {
+            ForEach(2..<100) {
+              Text("\($0) people")
             }
           }
         }
-        .pickerStyle(.segmented)
         
-        Toggle(isOn: $additionalSettings) {
-          Text("Additional settings")
+        Section(header: Text("What you want to give?")) {
+          Picker("% tip", selection: $tipPercentage) {
+            ForEach(0..<tipPercentages.count) {
+              Text("\(self.tipPercentages[$0])%")
+            }
+          }
+          .pickerStyle(.segmented)
         }
         
-        Button(action: {
-          //
-        }) {
-          Text("Save changes")
+        Section {
+          Text("\(totalPerPerson, specifier: "%.2f")")
         }
-        .disabled(!additionalSettings)
       }
-      .navigationBarTitle("Settings")
+      .navigationBarTitle("Tips calculator")
+      .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
