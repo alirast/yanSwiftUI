543da7b6da528b8c06854de16ffd85888dd4ae72
alirast <xdabnormal@gmail.com>
2023-11-06 14:17:23 +0300

    form (simple example)

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -9,26 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
   
+  @State private var colors = ["red", "green", "blue"]
+  @State private var selectedColor = 0
+  @State private var additionalSettings = false
+  
   var body: some View {
-    VStack {
-      Group {
-        Text("hey")
-        Text("hey")
-        Text("hey")
-        Text("hey")
+    NavigationView {
+      Form {
+        Picker(selection: $selectedColor, label: Text("Select a color")) {
+          ForEach(0..<colors.count) {
+            Text(self.colors[$0])
+          }
+        }
+        .pickerStyle(.segmented)
+        
+        Toggle(isOn: $additionalSettings) {
+          Text("Additional settings")
+        }
+        
+        Button(action: {
+          //
+        }) {
+          Text("Save changes")
+        }
       }
-      Group {
-        Text("hey")
-        Text("hey")
-        Text("hey")
-        Text("hey")
-      }
-      Group {
-        Text("hey")
-        Text("hey")
-        Text("hey")
-        Text("hey")
-      }
+      .navigationBarTitle("Settings")
     }
   }
 }
