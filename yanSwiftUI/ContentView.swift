07482af7cc52375f0c4fd39782e075839faf3639
alirast <xdabnormal@gmail.com>
2023-11-06 14:44:45 +0300

    alert (part 1)

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -9,51 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
   
-  @State private var checkAmount = ""
-  @State private var numberOfPeople = 2
-  @State private var tipPercentage = 0
-  
-  let tipPercentages = [0, 5, 10, 15, 20]
-  
-  private var totalPerPerson: Double {
-    let peopleCount = Double(numberOfPeople + 2)
-    let tipSelection = Double(tipPercentages[tipPercentage])
-    let orderAmount = Double(checkAmount) ?? 0
-    let tipValue = orderAmount / 100 * tipSelection
-    let grandTotal = orderAmount + tipValue
-    let amountPerPerson = grandTotal / peopleCount
-    
-    return amountPerPerson
-  }
+  @State private var showingAlert = false
 
   var body: some View {
-    NavigationView {
-      Form {
-        Section {
-          TextField("Amount", text: $checkAmount)
-          Picker("Number of people", selection: $numberOfPeople) {
-            ForEach(2..<100) {
-              Text("\($0) people")
-            }
-          }
-        }
-        
-        Section(header: Text("What you want to give?")) {
-          Picker("% tip", selection: $tipPercentage) {
-            ForEach(0..<tipPercentages.count) {
-              Text("\(self.tipPercentages[$0])%")
-            }
-          }
-          .pickerStyle(.segmented)
-        }
-        
-        Section {
-          Text("\(totalPerPerson, specifier: "%.2f")")
-        }
-      }
-      .navigationBarTitle("Tips calculator")
-      .navigationBarTitleDisplayMode(.inline)
+    Button {
+      self.showingAlert = true
+    } label: {
+      Text("show alert")
     }
+    .alert(isPresented: $showingAlert) {
+      Alert(title: Text("Hello swiftUI"),
+            message: Text("Some detail msg"),
+            dismissButton: .default(Text("ok")))
+    }
+
   }
 }
 
