1a6c2b16afaa91788d6bc0b834ec07bb38d79817
alirast <xdabnormal@gmail.com>
2023-11-06 15:01:08 +0300

    alert (part 4)

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -9,25 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
   
-  @State private var showingAlert = false
+  @State private var showingSheet = false
 
   var body: some View {
     Button {
-      self.showingAlert = true
+      self.showingSheet = true
     } label: {
-      Text("Show alert")
+      Text("Show action sheet")
     }
-    .alert(isPresented: $showingAlert) {
-      Alert(title: Text("Are u sure to delete it?"),
-            message: Text("No way back"),
-            primaryButton: .destructive(Text("Delete")) {
-        print("Deleting")
-      },
-            secondaryButton: .cancel())
-     
-    
+    .actionSheet(isPresented: $showingSheet) {
+      ActionSheet(title: Text("what u wanna do?"),
+                  message: Text("there's only one choice"),
+                  buttons: [.default(Text("dismiss sheet")), .cancel(), .destructive(Text("delete"))])
     }
-
   }
 }
 
