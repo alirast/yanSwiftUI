7bd29feedf7725b3b40a8b2ee905948498e4fa14
alirast <xdabnormal@gmail.com>
2023-11-06 14:48:57 +0300

    alert (part 3)

--- yanSwiftUI/ContentView.swift
+++ yanSwiftUI/ContentView.swift
@@ -19,19 +19,18 @@ struct ContentView: View {
       } label: {
         Text("show 1")
       }
+      .alert(isPresented: $showingAlert1) {
+        Alert(title: Text("One"), dismissButton: .cancel())
+      }
       
       Button {
         self.showingAlert2 = true
       } label: {
         Text("show 2")
       }
-    }
-    .alert(isPresented: $showingAlert1) {
-      Alert(title: Text("One"), dismissButton: .cancel())
-    }
-    
-    .alert(isPresented: $showingAlert2) {
-      Alert(title: Text("Two"), dismissButton: .cancel())
+      .alert(isPresented: $showingAlert2) {
+        Alert(title: Text("Two"), dismissButton: .cancel())
+      }
     }
   }
 }
